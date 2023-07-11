a0578a618f0fadcb7371fdf557381bf01d9af3ca
corei8 <38426287+corei8@users.noreply.github.com>
2023-07-11 16:50:14 -0400

    bump ordotools version

--- printcalendar.py
+++ printcalendar.py
@@ -11,18 +11,18 @@ class PrintCalendar:
         month = [[]]
         i = 0
         row = 0
-        for x in self.data:
-            if x.strftime("%B") == self.month:
+        for feast in self.data:
+            if feast.date.strftime("%B") == self.month:
                 if not month[0]:
-                    if int(x.strftime("%w")) != 0:
-                        pre_blanks = int(x.strftime("%w"))
+                    if int(feast.date.strftime("%w")) != 0:
+                        pre_blanks = int(feast.date.strftime("%w"))
                         for y in range(pre_blanks):
                             month[row].append([])
                             i += 1
                     else:
                         pass
-                month[row].append({int(x.strftime("%d").lstrip("0")): self.data[x]})
-                last = 6-int(x.strftime("%w"))
+                month[row].append({int(feast.date.strftime("%d").lstrip("0")): feast})
+                last = 6-int(feast.date.strftime("%w"))
                 i += 1
                 if i % 7 == 0:
                     month.append([])
