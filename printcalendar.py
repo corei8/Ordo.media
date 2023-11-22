07a511f48954f89f7bb7baa09338ffbeb13c5a11
corei8 <38426287+corei8@users.noreply.github.com>
2023-11-22 16:54:09 -0500

    upgrade ordotools

--- printcalendar.py
+++ printcalendar.py
@@ -19,16 +19,12 @@ class PrintCalendar:
                         for y in range(pre_blanks):
                             month[row].append([])
                             i += 1
-                    else:
-                        pass
                 month[row].append({int(feast.date.strftime("%d").lstrip("0")): feast})
                 last = 6-int(feast.date.strftime("%w"))
                 i += 1
                 if i % 7 == 0:
                     month.append([])
                     row += 1
-                else:
-                    pass
         for x in range(last):
             month[-1].append([])
 
