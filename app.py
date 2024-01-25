399118e091a5bb2d38c8e7cf9586e5eda5f14633
corei8 <38426287+corei8@users.noreply.github.com>
2024-01-25 09:34:33 -0500

    set some timers for server-side optimization metrics

--- app.py
+++ app.py
@@ -5,20 +5,37 @@ from printcalendar import PrintCalendar
 from datetime import datetime
 from flask_minify import Minify, decorators as minify_decorators
 
+import time
+import sys
+
 
 app = Flask(__name__)
 Minify(app=app, passive=True)
 
 
+
 @app.route("/")
 @minify_decorators.minify(html=True, js=True, cssless=True)
 def home():
+    start = time.time()
     year = int(datetime.today().strftime("%Y"))
     month = datetime.today().strftime("%B")
     raw = []
+    algo_start = time.time()
     for y in range(year-1,year+1):
         raw.extend(ordotools.LiturgicalCalendar(year=y, diocese="roman").build())
+    algo_end = time.time()
+    algo_elapse = algo_end - algo_start
+    print(f"Algorithm completed in {algo_elapse:.2f} seconds", file=sys.stderr)
+    data_start = time.time()
     data = PrintCalendar(month, raw).json_year
+    data_end = time.time()
+
+    data_elapse = data_end - data_start
+    print(f"Data formatting completed in {data_elapse:.2f} seconds", file=sys.stderr)
+    end = time.time()
+    elapse = end-start
+    print(f"Request completed in {elapse:.2f} seconds", file=sys.stderr)
     return render_template(
         "calendar.html",
         title="Calendar",
